53310215b2d0a58a036e68f6876ca5fc5de5874e
narasimhapeta <simhamp@gmail.com>
2021-07-13 23:13:41 +0530

    Basket API development

---
+++ src/Services/Basket/Basket.API/Controllers/APIControllerAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Basket.API.Controllers
+{
+  internal class APIControllerAttribute : Attribute
+  {
+  }
+}
---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Basket.API.Entities
+{
+  public class ShoppingCartItem
+  {
+    public int Quantity { get; set; }
+    public int Color { get; set; }
+    public int Price { get; set; }
+    public int ProductId { get; set; }
+    public int ProductName { get; set; }
+  }
+}
