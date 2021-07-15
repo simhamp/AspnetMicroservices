1026c8fd1ee0e2b206b2c619fbce0a4ff30e9c4a
narasimhapeta <simhamp@gmail.com>
2021-07-16 01:17:37 +0530

    Consuming Discount.Grpc from Basket Microservice and Dockerize Discount.Grpc

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,10 +16,12 @@ namespace Basket.API.Controllers
   {
 
     private readonly IBasketRepository _repository;
+    private readonly DiscountGrpcService _discountGrpcService;
 
-    public BasketController(IBasketRepository repository)
+    public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
     {
       _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+      _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
     }
 
     [HttpGet("{userName}", Name = "GetBasket")]
@@ -33,6 +36,14 @@ namespace Basket.API.Controllers
     [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
     public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
     {
+      // TODO: Communicate with Discount.Grpc
+      //and calculate latest price of product into shopping cart.
+      foreach(var item in basket.Items)
+      {
+        var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+        item.Price -= coupon.Amount;
+      }
+
       return Ok(await _repository.UpdateBasket(basket));
     }
 
